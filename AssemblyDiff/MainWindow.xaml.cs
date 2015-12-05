4c6d74383a0a8ec2eb18454f5564cbe2a3b47722
Nikolay Balakin <n@balakin.me>
2015-12-05 20:27:43 +0200

    Initial commit

---
+++ AssemblyDiff/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace Balakin.AssemblyDiff {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
