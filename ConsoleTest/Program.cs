782c6bc3fdcd66412a3b14c1869e842b8f84f4c3
Nikolay Balakin <n@balakin.me>
2015-12-07 19:31:33 +0200

    Add test console project

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Balakin.AssemblyDiff;
+
+namespace ConsoleTest {
+    class Program {
+        static void Main(string[] args) {
+            var diff = AssemblyDiff.Calculate(@"C:\Sources\GitHub\VSOutputEnhancer\2013_Balakin.VSOutputEnhancer.dll", @"C:\Sources\GitHub\VSOutputEnhancer\2015_Balakin.VSOutputEnhancer.dll");
+            Debugger.Break();
+        }
+    }
+}
